eec5a4f034e06858bd7f6945a167ad3f9d2110bf
Murto <murto@airmail.cc>
2019-03-16 20:47:27 +0000

    Add event class for actions driven by triggers

--- server/src/api/Event.py
+++ server/src/api/Event.py
@@ -1,3 +1,6 @@
+from Action import Action
+
+
 class Event:
 
   def __init__(self, id, name, trigger, actions):
@@ -17,3 +20,13 @@ class Event:
 
   def get_actions():
     return self.actions
+
+  def to_json(self):
+    return {'id':self.id, 'name':self.name, 'trigger':self.trigger, 'actions':[ action.to_json() for action in self.actions ]}
+
+  def from_json(event_json, id=None):
+    id = id or event_json['id']
+    name = event_json['name']
+    trigger = event_json['trigger']
+    actions = [ Action.from_json(action_json) for action_json in event_json['actions'] ]
+    return Event(id, name, trigger, actions)
