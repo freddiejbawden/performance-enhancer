efc457cfa5d7064fbc96688d24c330efb68d21c4
Murto <murto@airmail.cc>
2019-03-16 18:03:22 +0000

    Add routes for events

--- server/src/api/routes.py
+++ server/src/api/routes.py
@@ -7,22 +7,48 @@ import json
 def root():
   return "Hello, World!"
 
-@api.route('/api/events', methods=['GET']):
-def get_events():
+
+# Show API
+
+@api.route('/api/shows', methods=['GET'])
+def get_shows():
   return json.dumps([])
 
-@api.route('/api/events/<int:id>', methods=['GET'])
-def get_event(id):
+@api.route('/api/shows/<int:show_id>', methods=['GET'])
+def get_show(show_id):
   return json.dumps({})
 
-@api.route('/api/events', methods=['POST']):
-def make_event():
+@api.route('/api/shows', methods=['POST'])
+def make_show():
   return json.dumps({}), 201
 
-@api.route('/api/events/<int:id>', methods=['PUT']):
-def update_event(id):
+@api.route('/api/shows/<int:show_id>', methods=['PUT'])
+def update_show(show_id):
   return json.dumps({})
 
-@api.route('/api/events/<int:id>', methods=['DELETE']):
-def delete_event(id):
-  return json.dumps({'result':True})  
+@api.route('/api/shows/<int:show_id>', methods=['DELETE'])
+def delete_show(show_id):
+  return json.dumps({'result':True})
+
+
+# Event API
+
+@api.route('/api/shows/<int:show_id>/events', methods=['GET'])
+def get_events(show_id)
+  return json.dumps([])
+
+@api.route('/api/shows/<int:show_id>/events/<int:event_id>', methods=['GET'])
+def get_event(show_id, event_id):
+  return json.dumps({})
+
+@api.route('/api/shows/<int:show_id>/events', methods=['POST'])
+def make_show(show_id):
+  return json.dumps({}), 201
+
+@api.route('/api/shows/<int:show_id>/events/<int:event_id>', methods=['PUT'])
+def update_show(show_id, event_id):
+  return json.dumps({})
+
+@api.route('/api/shows/<int:show_id>/events/<int:event_id>', methods=['DELETE'])
+def delete_show(show_id, event_id):
+  return json.dumps({'result':True})
