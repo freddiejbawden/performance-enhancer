e7a93ae4a014e511f87f0f82a8a6904be8f6a221
Murto <murto@airmail.cc>
2019-03-16 20:47:48 +0000

    Add show class, representative of a fringe show

--- server/src/api/Show.py
+++ server/src/api/Show.py
@@ -1,4 +1,6 @@
 from Event import Event
+from IDGenerator import IDGenerator
+
 
 class Show:
 
@@ -6,13 +8,41 @@ class Show:
     self.id = id
     self.name = name
     self.events = events
+    self.event_id_gen = IDGenerator(0)
     
-
-  def get_id():
+  def get_id(self):
     return self.id
 
-  def get_name():
+  def get_name(self):
     return self.name
 
-  def get_events():
+  def get_events(self):
     return self.events
+
+  def get_event(self, id):
+    if (id in self.events):
+      return self.events[id]
+    return None
+
+  def add_event_from_json(self, event_json):
+    event = Event.from_json(event_json, self.event_id_gen.next())
+    self.events[event.get_id()] = event
+    return event
+
+  def update_event(self, event):
+    if (not event in self.events):
+      return None
+    self.events[event.get_id()] = event
+
+  def remove_event(self, id):
+    if (id in self.events):
+      self.events.pop(id)
+
+  def to_json(self):
+    return {'id':self.id, 'name':self.name, 'events':[ event.to_json() for event in self.events ]}
+
+  def from_json(show_json, id=None):
+    id = id or show_json['id']
+    name = show_json['name']
+    events = [ Event.from_json(event_json) for event_json in show_json['events'] ]
+    return Show(id, name, events)
