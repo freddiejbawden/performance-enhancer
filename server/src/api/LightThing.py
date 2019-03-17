3694bb2370aa5722c55d7fcecf0c9a65e25a35e9
Murto <murto@airmail.cc>
2019-03-17 04:54:50 +0000

    Create classes for simple switch and light devices

---
+++ server/src/api/LightThing.py
@@ -0,0 +1,35 @@
+class LightThing:
+  
+  def __init__(self, id, address, levels, level):
+    self.type = "LIGHT"
+    self.id = id
+    self.address = address
+    self.levels = levels
+    self.level = level
+
+  def get_type(self):
+    return self.type
+
+  def get_id(self):
+    return self.id
+
+  def get_address(self):
+    return self.address
+
+  def get_levels(self):
+    return self.levels
+
+  def get_level(self):
+    return self.level
+
+  def set_level(level):
+    self.level = level
+
+  def to_json(self):
+    return {'type':self.type, 'id':self.id, 'address':self.address, 'level':self.level, 'levels':self.levels}
+
+  def from_json(thing_json, thing_id):
+    address = thing_json['address']
+    level = thing_json['level']
+    levels = thing_json['levels']
+    return LightThing(thing_id, address, levels, level)
---
+++ server/src/api/SwitchThing.py
@@ -0,0 +1,34 @@
+class SwitchThing:
+
+  def __init__(self, id, address, on):
+    self.type = "SWITCH"
+    self.id = id
+    self.address = address
+    self.on = on
+
+  def get_type(self):
+    return self.type
+
+  def get_id(self):
+    return self.id
+
+  def get_address(self):
+    return self.address
+
+  def get_on(self):
+    return self.on
+
+  def set_on(self):
+    self.on = True
+
+  def set_off(self):
+    self.on = False
+
+  def to_json(self):
+    return {'type':self.type, 'id':self.id, 'address':self.address, 'on':self.on}
+
+  def from_json(thing_json, thing_id):
+    address = thing_json['address']
+    on = thing_json['on']
+    return SwitchThing(thing_id, address, on)
+    
