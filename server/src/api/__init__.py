f24a7b54d568a200ab6261ac2afe53aaca3a4d62
Murto <murto@airmail.cc>
2019-03-17 11:56:48 +0000

    Final commit

--- server/src/api/__init__.py
+++ server/src/api/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 api = Flask(__name__)
+cors = CORS(api)
+api.config['CORS_HEADERS'] = 'Content-Type'
 
 from api import routes
 
